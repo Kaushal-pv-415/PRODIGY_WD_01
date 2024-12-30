953e7c9fe083712b0c069b7747fcdae75e1475d1
Kaushal-pv-415 <kaushal.pv9@gmail.com>
2024-12-30 12:49:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select the navigation bar
+const navbar = document.getElementById('navbar');
+
+// Event listener for scroll effect
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
